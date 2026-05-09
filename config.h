73c7180b717e22759442ede150a133b453a93e05
FabBo23 <79717408+FabBo23@users.noreply.github.com>
2026-05-09 18:07:22 +0200

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 // ========================= Firmware-Version =========================
 // Bitte bei jeder Veröffentlichung erhöhen und einen Git-Tag (z.B. v1.0.1) erstellen.
-#define FIRMWARE_VERSION "1.0.15"
+#define FIRMWARE_VERSION "1.0.16"
 
 // ========================= USB-Serial Debug =========================
 // 0 (Default): keine Debug-Ausgaben über USB-Serial. Saubere Konsole, kein
